99df6b1cf45f1cfb117213281af02319ceebda16
Suhib albayary <60950298+sohaib-albyari@users.noreply.github.com>
2023-11-21 18:09:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,89 +1,108 @@
 var width = window.innerWidth;
-      var height = window.innerHeight;
+var height = window.innerHeight;
+var conformation = document.querySelector(".overlay");
+var remove = document.querySelector("#remove");
+var cancel = document.querySelector("#cancel");
+var items = document.querySelector("#clip");
+var stage = new Konva.Stage({
+    container: "container",
+    width: width,
+    height: height,
+});
 
-      var stage = new Konva.Stage({
-        container: "container",
-        width: width,
-        height: height,
-      });
+var layer = new Konva.Layer();
+stage.add(layer);
 
-      var layer = new Konva.Layer();
-      stage.add(layer);
+var stageObj = new Image();
+stageObj.onload = function() {
+    var stages = new Konva.Image({
+        x: 0,
+        y: 0,
+        image: stageObj,
+        width: stage.width(),
+        height: stage.height(),
+    });
 
-      var stageObj = new Image();
-      stageObj.onload = function () {
-        var stages = new Konva.Image({
-          x: 0,
-          y: 0,
-          image: stageObj,
-          width: stage.width(),
-          height: stage.height(),
-        });
+    // add the shape to the layer
+    layer.add(stages);
+};
+stageObj.src = "stage.png";
 
-        // add the shape to the layer
-        layer.add(stages);
-      };
-      stageObj.src = "stage.png";
-
-      var item = (document.querySelector("#clip").onchange = (e) => {
-        var imageObj = new Image();
-        imageObj.onload = function () {
-          var yoda = new Konva.Image({
+items.onchange = (e) => {
+    var imageObj = new Image();
+    imageObj.onload = function() {
+        var item = new Konva.Image({
             x: 100,
             y: 100,
             image: imageObj,
             draggable: true,
-          });
+        });
 
-          // add the shape to the layer
-          layer.add(yoda);
+        // add the shape to the layer
+        layer.add(item);
 
-          // change zindex
-          yoda.on("dragstart", function () {
+        // change zindex
+        item.on("dragstart", function() {
             this.moveToTop();
             layer.draw();
-          });
+        });
 
-          // delete item
+        // delete item
+        item.on("dblclick dbltap", function(e) {
+            conformation.style.display = "block";
+            remove.addEventListener("click", function() {
+                item.destroy();
+                conformation.style.display = "none";
+                item.off("dblclick dbltap");
 
-          yoda.on("dblclick dbltap", function () {
-            this.destroy();
+            });
+            cancel.addEventListener("click", function() {
+                item.removeEventListener();
+                item.off("dblclick dbltap");
+
+                conformation.style.display = "none";
+            });
+            console.log(e.type);
             layer.draw();
-          });
-        };
-        imageObj.src = e.target.value;
-        e.target.value = "";
-      });
+        });
 
-      // alternative API:
-      //   Konva.Image.fromURL(
-      //     'https://konvajs.org//assets/darth-vader.jpg',
-      //     function (darthNode) {
-      //       darthNode.setAttrs({
-      //         x: 200,
-      //         y: 50,
-      //         scaleX: 0.5,
-      //         scaleY: 0.5,
-      //       });
-      //       layer.add(darthNode);
-      //     }
-      //   );
 
-      function downloadURI(uri, name) {
-        var link = document.createElement("a");
-        link.download = name;
-        link.href = uri;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        delete link;
-      }
+    };
+    imageObj.src = e.target.value;
+    e.target.value = "";
+};
 
-      document.getElementById("save").addEventListener(
-        "click",
-        function () {
-          var dataURL = stage.toDataURL({ pixelRatio: 3 });
-          downloadURI(dataURL, "stage.png");
-        },
-        false
-      );
+// alternative API:
+//   Konva.Image.fromURL(
+//     'https://konvajs.org//assets/darth-vader.jpg',
+//     function (darthNode) {
+//       darthNode.setAttrs({
+//         x: 200,
+//         y: 50,
+//         scaleX: 0.5,
+//         scaleY: 0.5,
+//       });
+//       layer.add(darthNode);
+//     }
+//   );
+
+function downloadURI(uri, name) {
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    delete link;
+}
+
+document.getElementById("save").addEventListener(
+    "click",
+    function() {
+        var dataURL = stage.toDataURL({
+            pixelRatio: 3
+        });
+        downloadURI(dataURL, "stage.png");
+    },
+    false
+);
